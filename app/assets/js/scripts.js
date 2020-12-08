d61ba3f8ff992edf8710803f7734500987d4e863
“Frazer” <frazer.findlater@gmail.com>
2020-12-09 09:14:29 +1300

    icebreaker-annual-report-concept: Initial commit.

---
+++ app/assets/js/scripts.js
@@ -0,0 +1,19 @@
+/*!
+ * fastshell
+ * Fiercely quick and opinionated front-ends
+ * https://HosseinKarami.github.io/fastshell
+ * @author Hossein Karami
+ * @version 1.0.5
+ * Copyright 2020. MIT licensed.
+ */
+(function ($, window, document, undefined) {
+  "use strict";
+
+  $(document).ready(function () {
+    $(".hamburger").click(function () {
+      $(this).toggleClass("open");
+      $(".mobile-menu").fadeToggle();
+      $("body").toggleClass("overflow-hidden");
+    });
+  });
+})(jQuery, window, document);
---
+++ src/js/scripts.js
@@ -0,0 +1,11 @@
+(function ($, window, document, undefined) {
+  "use strict";
+
+  $(document).ready(function () {
+    $(".hamburger").click(function () {
+      $(this).toggleClass("open");
+      $(".mobile-menu").fadeToggle();
+      $("body").toggleClass("overflow-hidden");
+    });
+  });
+})(jQuery, window, document);
